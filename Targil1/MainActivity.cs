5df94915b87c62769203464b0f107de320c1dcba
Home <Home@LAPTOP-2E6V6ACM>
2022-01-16 17:44:50 +0200

    added comment

--- Targil1/MainActivity.cs
+++ Targil1/MainActivity.cs
@@ -13,6 +13,7 @@ namespace Targil1
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
+            //comment
             SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
